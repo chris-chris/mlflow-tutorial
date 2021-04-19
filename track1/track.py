9cd7bacf7d32fc7f668f7de46d408a4897fb04e0
Chris Song <chris@Chrisui-MacBookPro.local>
2021-04-19 12:10:44 +0900

    add lessons

---
+++ track1/track.py
@@ -0,0 +1,19 @@
+import os
+from random import random, randint
+from mlflow import log_metric, log_param, log_artifacts
+
+if __name__ == "__main__":
+    # Log a parameter (key-value pair)
+    log_param("param1", randint(0, 100))
+
+    # Log a metric; metrics can be updated throughout the run
+    log_metric("foo", random())
+    log_metric("foo", random() + 1)
+    log_metric("foo", random() + 2)
+
+    # Log an artifact (output file)
+    if not os.path.exists("outputs"):
+        os.makedirs("outputs")
+    with open("outputs/test.txt", "w") as f:
+        f.write("hello world!")
+    log_artifacts("outputs")
